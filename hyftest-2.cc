2e405735d7d5fdf8d4c153157e7eb93c8992fe4a
siyuenyip <1781197828@qq.com>
2020-05-19 11:38:12 +0800

    date 5-19

--- hyftest-2.cc
+++ hyftest-2.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int camp(int a, int b)
 {
@@ -9,27 +10,91 @@ int camp(int a, int b)
     else
         return 0;
 }
-int main()
+string changeC2N(string str)
 {
-    int n, i, j;
-    cout << "print n:";
-    cin >> n;
-    int *a = new int[n];
-    for (i = 0; i < n; i++)
+    int i = 0;
+    string s;
+    while (str[i])
     {
-        cin >> a[i];
-        // cout << a[i];
+        switch (str[i])
+        {
+        case 'A':
+            s = s + '1';
+            break;
+        case 'T':
+            s = s + '2';
+            break;
+        case 'G':
+            s = s + '3';
+            break;
+        case 'C':
+            s = s + '4';
+            break;
+        default:
+            break;
+        }
+        i++;
     }
-    // cout << endl;
+    return s;
+}
+int **change2M(string str)
+{
+    int i, j,
+        n = str.length();
+    int **arr = new int *[n];
     for (i = 0; i < n; i++)
     {
+        arr[i] = new int[n];
         for (j = 0; j < n; j++)
         {
             // cout << a[i] << a[j] << " : ";
-            cout << camp(a[i], a[j]) << " ";
+            cout << camp(str[i], str[j]) << " ";
+            arr[i][j] = camp(str[i], str[j]);
         }
         cout << endl;
     }
+    return arr;
+}
+double dSL(int **a, int **b, int m, int n)
+{
+    double res;
+    int sum = 0;
+    if (m == n)
+    {
+        cout << "if two string length is equal,this is the end" << endl;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                sum += pow(abs(a[i][j] - b[i][j]), 2);
+            }
+        res = sqrt(sum) / (2 * n * (n - 1));
+    }
+    else
+    {
+    }
+    return res;
+}
+
+int main()
+{
+    int n, i, j;
+    string str1, str2;
+    cout << "print str1:";
+
+    cin >> str1;
+    // str1 = "GCTA";
+    // str2 = "ATGC";
+    cout << "print str2:";
+    cin >> str2;
+    // cout << str1 << "\t" << changeC2N(str1) << "\t" << str1.length() << endl;
+    cout << "A 矩阵结果" << endl;
+    int **a = change2M(changeC2N(str1));
+    cout << "B 矩阵结果" << endl;
+    int **b = change2M(changeC2N(str2));
+    // cout << a[2][3] + a[1][3] << endl;
+    // cout << abs(-90) << endl;
+
+    cout << dSL(a, b, str1.length(), str2.length()) << endl;
 
     return 0;
 }
